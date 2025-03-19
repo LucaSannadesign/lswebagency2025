f9b56edc8e880fe81ac0a70569f2bf59565d2d81
ls-webagency2025 <info@lucasanna.eu>
2025-03-19 17:22:06 +0100

    Fix caricamento immagini e ottimizzazione permalink

--- src/utils/permalinks.ts
+++ src/utils/permalinks.ts
@@ -1,7 +1,5 @@
 import slugify from 'limax';
-
 import { SITE, APP_BLOG } from 'astrowind:config';
-
 import { trim } from '~/utils/utils';
 
 export const trimSlash = (s: string) => trim(trim(s, '/'));
@@ -25,7 +23,9 @@ export const BLOG_BASE = cleanSlug(APP_BLOG?.list?.pathname);
 export const CATEGORY_BASE = cleanSlug(APP_BLOG?.category?.pathname);
 export const TAG_BASE = cleanSlug(APP_BLOG?.tag?.pathname) || 'tag';
 
-export const POST_PERMALINK_PATTERN = trimSlash(APP_BLOG?.post?.permalink || `${BLOG_BASE}/%slug%`);
+export const POST_PERMALINK_PATTERN = trimSlash(
+  (APP_BLOG?.post?.permalink ? APP_BLOG.post.permalink.replace(/^\/blog\/?/, '') : '') || `${BLOG_BASE}/%slug%`
+);
 
 /** */
 export const getCanonical = (path = ''): string | URL => {
@@ -56,33 +56,26 @@ export const getPermalink = (slug = '', type = 'page'): string => {
     case 'home':
       permalink = getHomePermalink();
       break;
-
     case 'blog':
       permalink = getBlogPermalink();
       break;
-
     case 'asset':
       permalink = getAsset(slug);
       break;
-
     case 'category':
       permalink = createPath(CATEGORY_BASE, trimSlash(slug));
       break;
-
     case 'tag':
       permalink = createPath(TAG_BASE, trimSlash(slug));
       break;
-
     case 'post':
       permalink = createPath(trimSlash(slug));
       break;
-
     case 'page':
     default:
       permalink = createPath(slug);
       break;
   }
-
   return definitivePermalink(permalink);
 };
 
