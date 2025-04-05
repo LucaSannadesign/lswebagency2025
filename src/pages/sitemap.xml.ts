a84f7aa57a0534d7a813b9fd13f7cba25db2ea63
ls-webagency2025 <info@lucasanna.eu>
2025-04-05 09:55:10 +0200

    Fix sitemap URL duplicati e struttura finale corretta

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -3,11 +3,9 @@ import { getCollection } from 'astro:content';
 export async function GET() {
   const baseUrl = 'https://lswebagency.com';
 
-  // Recupera contenuti da collezioni
   const posts = await getCollection('post');
   const portfolioItems = await getCollection('portfolio');
 
-  // Pagine statiche principali
   const staticPages = [
     '/',
     '/chi-siamo',
@@ -17,13 +15,13 @@ export async function GET() {
     '/blog',
   ];
 
-  // Costruzione sitemap XML
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${staticPages.map((page) => `<url><loc>${baseUrl}${page}</loc></url>`).join('')}
   ${posts.map((post) => {
-    const slug = post.data.slug || post.id;
-    return `<url><loc>${baseUrl}/blog/${slug}</loc></url>`;
+    let rawSlug = post.data.slug || post.id;
+    rawSlug = rawSlug.replace(/^\/?blog\/?/, '').replace(/^\/+|\/+$/g, '');
+    return `<url><loc>${baseUrl}/blog/${rawSlug}</loc></url>`;
   }).join('')}
   ${portfolioItems.map((item) => {
     const slug = item.data.slug || item.id;
