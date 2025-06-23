61d1f097a786e835b1e050aad074eed6289670b9
ls-webagency2025 <info@lucasanna.eu>
2025-06-23 09:46:16 +0200

    ⏪ Ripristino Header e navigation allo stato precedente: menu mobile funzionante

--- src/navigation.ts
+++ src/navigation.ts
@@ -4,33 +4,30 @@ export const headerData = {
     { text: 'Chi Siamo', href: '/chi-siamo' },
     {
       text: 'Servizi',
-      href: '/servizi',              // ← aggiunto qui
       links: [
-        { text: 'Creazione siti web',      href: '/servizi/creazione-siti-web-sassari' },
+        { text: 'Creazione siti web', href: '/servizi/creazione-siti-web-sassari' },
         { text: 'Realizzazione E-commerce', href: '/servizi/realizzazione-siti-ecommerce' },
-        { text: 'Branding e Grafica',       href: '/servizi/branding-e-grafica-siti-web' },
-        { text: 'Ottimizzazione SEO',       href: '/servizi/ottimizzazione-seo-siti-web' },
-        { text: 'Voucher Digitali',         href: '/servizi/voucher-digitali-sassari' },
+        { text: 'Branding e Grafica', href: '/servizi/branding-e-grafica-siti-web' },
+        { text: 'Ottimizzazione SEO', href: '/servizi/ottimizzazione-seo-siti-web' },
+        { text: 'Voucher Digitali', href: '/voucher-digitali-sardegna' } // Nuova voce aggiunta
       ],
     },
     { text: 'Portfolio', href: '/portfolio' },
-    { text: 'Blog',      href: '/blog' },
-    { text: 'Contatti',  href: '/contatti' },
+    { text: 'Blog', href: '/blog' },
+    { text: 'Contatti', href: '/contatti' },
   ],
-  actions: [
-    { text: 'Richiedi un Preventivo', href: '/contatti' },
-  ],
+  actions: [{ text: 'Richiedi un Preventivo', href: '/contatti' }],
 };
 
 export const footerData = {
   links: [
-    { text: 'Privacy Policy',     href: '/privacy' },
+    { text: 'Privacy Policy', href: '/privacy' },
     { text: 'Termini di Servizio', href: '/terms' },
-    { text: 'Voucher Digitali',    href: '/servizi/voucher-digitali-sassari' },
+    { text: 'Voucher Digitali', href: '/voucher-digitali-sardegna' } // Nuovo link nel footer
   ],
   socialLinks: [
-    { ariaLabel: 'Linkedin',  icon: 'tabler:brand-linkedin',  href: 'https://www.linkedin.com/company/ls-web-agency' },
-    { ariaLabel: 'Facebook',  icon: 'tabler:brand-facebook',  href: 'https://www.facebook.com/lswebdesignagency' },
+    { ariaLabel: 'Linkedin', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/company/ls-web-agency' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/lswebdesignagency' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/ls_web_agency' },
   ],
   secondaryLinks: [],
